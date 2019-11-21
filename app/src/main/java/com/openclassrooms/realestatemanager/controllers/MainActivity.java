a6679e9cafb20dd8ee6110bb74f2748388a1c268
e210564e <devedelie@gmail.com>
2019-11-21 15:39:41 +0100

    - Refactoring of files into dedicated packages

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/MainActivity.java
@@ -1,8 +1,11 @@
-package com.openclassrooms.realestatemanager;
+package com.openclassrooms.realestatemanager.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
