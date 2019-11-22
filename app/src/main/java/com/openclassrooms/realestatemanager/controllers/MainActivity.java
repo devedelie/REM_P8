7b36fae8cc4d2e2e503737e0d3623c4ee72f6b8e
e210564e <devedelie@gmail.com>
2019-11-22 18:34:29 +0100

    - Added ActionBar with 3 icons (add, edit, search) - Added navigationDrawer with its drawer layout - Added new dependencies

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/MainActivity.java
@@ -1,25 +1,48 @@
 package com.openclassrooms.realestatemanager.controllers;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.utils.Utils;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView textViewMain;
     private TextView textViewQuantity;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.main_activity_drawerLayout) DrawerLayout drawerLayout;
+    @BindView(R.id.drawer_main_activity) NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this); //Configure Butterknife
 
         this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
+        this.configureToolbar();
+        this.configureDrawerLayoutAndNavigationView();
         this.configureTextViewMain();
         this.configureTextViewQuantity();
     }
@@ -33,5 +56,51 @@ public class MainActivity extends AppCompatActivity {
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
         this.textViewQuantity.setText(String.valueOf(quantity));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+
+    private void configureToolbar(){
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+    }
+
+    // Navigation drawer config
+    protected void configureDrawerLayoutAndNavigationView(){
+        // Configure drawer layout
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Configure NavigationView & set item selection listener
+        View headerView = this.navigationView.getHeaderView(0);
+        TextView userName = headerView.findViewById(R.id.navigation_header_name);
+        TextView userEmail = headerView.findViewById(R.id.navigation_header_email);
+
+
+        // Set listener
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int order = menuItem.getOrder();
+        Log.d(TAG, "Test onNavigationItemSelected: "+ order);
+        switch (order){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
