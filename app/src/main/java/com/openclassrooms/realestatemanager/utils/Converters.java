2180a91ff48a7e667ada8fb30c955133535c86e3
e210564e <devedelie@gmail.com>
2019-12-11 18:16:53 +0100

    - Added TypeConverter to manipulate data from/to database

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/Converters.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/Converters.java
@@ -1,8 +1,16 @@
 package com.openclassrooms.realestatemanager.utils;
 
+import android.util.Log;
+
 import androidx.room.TypeConverter;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Eliran Elbaz on 10-Dec-19.
@@ -16,6 +24,38 @@ public class Converters {
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
+    }
+
+    @TypeConverter
+    public static ArrayList<String> fromString(String value) {
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static String fromArrayList(ArrayList<String> list) {
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        return json;
+    }
+
+    @TypeConverter
+    public static Map<String,String> fromStringToMapStringString(String value) {
+        if (value == null){
+            return(null);
+        }
+        Type mapType = new TypeToken<Map<String,String>>() {}.getType();
+        return new Gson().fromJson(value, mapType);
+    }
+
+    @TypeConverter
+    public static String fromMapStringStringToString(Map<String,String> map) {
+        if (map == null){
+            return(null);
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson(map);
+        return json;
     }
 }
 
