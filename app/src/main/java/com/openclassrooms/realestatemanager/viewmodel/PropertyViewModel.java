b15b9916fd72f5c78bcc72230328dec9cc8c8281
e210564e <devedelie@gmail.com>
2019-12-11 18:22:28 +0100

    - Added new methods to apply actions on the database

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/PropertyViewModel.java
@@ -5,7 +5,9 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.openclassrooms.realestatemanager.models.Property;
+import com.openclassrooms.realestatemanager.models.User;
 import com.openclassrooms.realestatemanager.repositories.PropertyDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UserDataRepository;
 
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -15,24 +17,51 @@ import java.util.concurrent.Executor;
  */
 public class PropertyViewModel extends ViewModel {
     // REPOSITORIES
-    private final PropertyDataRepository mPropertyDataRepository;
+    private final PropertyDataRepository mPropertyDataSource;
+    private final UserDataRepository mUserDataSource;
     private final Executor executor;
 
     // DATA
     @Nullable
-    private LiveData<Property> property;
+    private LiveData<User> currentUser;
 
-    public PropertyViewModel(PropertyDataRepository propertyDataRepository, Executor executor) {
-        mPropertyDataRepository = propertyDataRepository;
+    public PropertyViewModel(PropertyDataRepository propertyDataSource, UserDataRepository userDataSource, Executor executor) {
+        this.mPropertyDataSource = propertyDataSource;
+        this.mUserDataSource = userDataSource;
         this.executor = executor;
     }
+
+    public void init(long userId) {
+        if (this.currentUser != null) {
+            return;
+        }
+        currentUser = mUserDataSource.getUser(userId);
+    }
+
+    // -------------
+    // FOR USER
+    // -------------
+
+    public LiveData<User> getUser(long userId) { return this.currentUser;  }
 
     // -------------
     // For Property
     // -------------
 
     public LiveData<List<Property>> getProperties() {
-        return mPropertyDataRepository.getProperties();
+        return mPropertyDataSource.getProperties();
+    }
+
+    public void createProperty(Property property) {
+        executor.execute(() -> {
+            mPropertyDataSource.createProperty(property);
+        });
+    }
+
+    public void updateProperty(Property property) {
+        executor.execute(() -> {
+            mPropertyDataSource.updateProperty(property);
+        });
     }
 
 
