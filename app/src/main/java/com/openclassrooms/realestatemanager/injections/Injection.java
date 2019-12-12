911f0b50a192ee3fc33b4eb2ebdd3c1dfb67c7e0
e210564e <devedelie@gmail.com>
2019-12-13 00:30:40 +0100

    - Added user data source to ViewModel & Injection

--- app/src/main/java/com/openclassrooms/realestatemanager/injections/Injection.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/injections/Injection.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.openclassrooms.realestatemanager.database.RealEstateManagerDatabase;
 import com.openclassrooms.realestatemanager.repositories.PropertyDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UserDataRepository;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -18,13 +19,19 @@ public class Injection {
         return new PropertyDataRepository(database.mPropertyDao());
     }
 
+    public static UserDataRepository provideUserDataSource(Context context) {
+        RealEstateManagerDatabase database = RealEstateManagerDatabase.getInstance(context);
+        return new UserDataRepository(database.mUserDao());
+    }
+
     public static Executor provideExecutor(){
         return Executors.newSingleThreadExecutor();
     }
 
     public static ViewModelFactory provideViewModelFactory(Context context) {
         PropertyDataRepository dataSourceProperty = providePropertyDataSource(context);
+        UserDataRepository dataSourceUser = provideUserDataSource(context);
         Executor executor = provideExecutor();
-        return new ViewModelFactory(dataSourceProperty, executor);
+        return new ViewModelFactory(dataSourceProperty, dataSourceUser, executor);
     }
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/injections/ViewModelFactory.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/injections/ViewModelFactory.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.openclassrooms.realestatemanager.repositories.PropertyDataRepository;
+import com.openclassrooms.realestatemanager.repositories.UserDataRepository;
 import com.openclassrooms.realestatemanager.viewmodel.PropertyViewModel;
 
 import java.util.concurrent.Executor;
@@ -14,11 +15,13 @@ import java.util.concurrent.Executor;
  */
 public class ViewModelFactory implements ViewModelProvider.Factory {
 
-    private final PropertyDataRepository mPropertyDataRepository;
+    private final PropertyDataRepository mPropertyDataSource;
+    private final UserDataRepository mUserDataSource;
     private final Executor executor;
 
-    public ViewModelFactory(PropertyDataRepository propertyDataRepository, Executor executor) {
-        mPropertyDataRepository = propertyDataRepository;
+    public ViewModelFactory(PropertyDataRepository propertyDataSource, UserDataRepository userDataSource,  Executor executor) {
+        this.mPropertyDataSource = propertyDataSource;
+        this.mUserDataSource = userDataSource;
         this.executor = executor;
     }
 
@@ -26,7 +29,7 @@ public class ViewModelFactory implements ViewModelProvider.Factory {
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         if (modelClass.isAssignableFrom(PropertyViewModel.class)) {
-            return (T) new PropertyViewModel(mPropertyDataRepository, executor);
+            return (T) new PropertyViewModel(mPropertyDataSource, mUserDataSource, executor);
         }
         throw new IllegalArgumentException("Unknown ViewModel class");
     }
