df88f006cf63a948d5db8fcfc955c0fc2a6b2032
e210564e <devedelie@gmail.com>
2019-12-17 20:20:01 +0100

    - Added Toolbar for DetailActivity.class for cases when the app is being launched on smartphone - Added methods to detect back-press & home button press

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/DetailActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/DetailActivity.java
@@ -1,14 +1,20 @@
 package com.openclassrooms.realestatemanager.controllers.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.fragments.DetailFragment;
 
-public class DetailActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class DetailActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
     // Declare detail fragment
     private DetailFragment detailFragment;
 
@@ -16,9 +22,42 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this); //Configure Butterknife
+
+        this.configureToolbar();
 
         // Configure and show home fragment
         this.configureAndShowDetailFragment();
+    }
+
+    private void configureToolbar(){
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    //------------
+    // ACTIONS
+    //------------
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    // Detect the click on "back" button and finish the current activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if ( id == android.R.id.home ) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     // --------------
