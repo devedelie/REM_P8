c6c320aba19c37213a9332176c75149a42d247de
e210564e <devedelie@gmail.com>
2019-12-27 16:31:30 +0100

    - Modified permissions logic and callbacks

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/SplashScreen.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/SplashScreen.java
@@ -1,24 +1,27 @@
 package com.openclassrooms.realestatemanager.controllers.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 import com.openclassrooms.realestatemanager.R;
 
 import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class SplashScreen extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
     private static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
-    private static final int RC_PERMISSION_CODE = 100;
-    public static Boolean mDiskPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +32,16 @@ public class SplashScreen extends AppCompatActivity implements EasyPermissions.P
     }
 
     /**
-     * Method to ask the user for Disk_READ authorization (with EasyPermissions support)
+     * Method to ask the user for Disk_READ_WRITE authorization (with EasyPermissions support)
      */
+    @AfterPermissionGranted(100)
     private void askPermission() {
-        if (!EasyPermissions.hasPermissions(this, READ_EXTERNAL_STORAGE)) {
-            EasyPermissions.requestPermissions(this, getString(R.string.popup_title_permission_disk_access), RC_PERMISSION_CODE, READ_EXTERNAL_STORAGE);
-            return;
-        } else {
-            mDiskPermissionGranted = true;
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE};
+        if(EasyPermissions.hasPermissions(this, perms)){
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
             launchMainActivity();
+        }else {
+            EasyPermissions.requestPermissions(this, getString(R.string.popup_title_permission_disk_access), 100, perms);
         }
     }
 
@@ -49,22 +53,30 @@ public class SplashScreen extends AppCompatActivity implements EasyPermissions.P
 
     @Override
     public void onPermissionsGranted(int requestCode, List<String> perms) {
-        mDiskPermissionGranted = true;
         launchMainActivity();
 
 
     }
     @Override
     public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
-        mDiskPermissionGranted = false;
-        recreate();
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            new AppSettingsDialog.Builder(this).build().show();
+        }else {
+            recreate();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+
+        }
     }
 
     private void launchMainActivity(){
-        if(mDiskPermissionGranted){
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
-        }
     }
 }
