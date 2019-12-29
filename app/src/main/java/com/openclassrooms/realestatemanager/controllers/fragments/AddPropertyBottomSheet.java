2b7c9ff3a3329b52aa20a14f9b0b9b0ae4834002
e210564e <devedelie@gmail.com>
2019-12-29 14:59:23 +0100

    - Added functionality for dismissing the bottomSheet with alertDialog

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/AddPropertyBottomSheet.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/AddPropertyBottomSheet.java
@@ -2,6 +2,7 @@ package com.openclassrooms.realestatemanager.controllers.fragments;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,9 +11,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.base.BaseBottomSheet;
+import com.openclassrooms.realestatemanager.models.Property;
+import com.openclassrooms.realestatemanager.viewmodel.PropertyViewModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,12 +30,13 @@ import static com.openclassrooms.realestatemanager.models.Constants.BOTTOM_SHEET
  * Created by Eliran Elbaz on 26-Dec-19.
  */
 public class AddPropertyBottomSheet extends BaseBottomSheet {
-    @BindView(R.id.test_bottom_sheet) TextView test;
+//    @BindView(R.id.test_bottom_sheet) TextView test;
     // Bundle
     private static final String CURRENT_PROPERTY_ID = "CURRENT_PROPERTY_ID";
     // DATA
     private int currentPropertyID;
     private BottomSheetListener mListener;
+    private PropertyViewModel mPropertyViewModel;
 
     public static AddPropertyBottomSheet newInstance(int currentPropertyID) {
         AddPropertyBottomSheet addPropertyBottomSheet = new AddPropertyBottomSheet();
@@ -84,15 +90,55 @@ public class AddPropertyBottomSheet extends BaseBottomSheet {
 
     @OnClick(R.id.x_button)
     public void onClickXButton(){
-        mListener.onClosedBottomSheet(BOTTOM_SHEET_ADD_TAG, "some data");
+        alertDialogXButton();
+    }
+
+//    @OnClick(R.id.add_property)
+//    public void onClickAddButton(){
+//        // mListener will notify MainActivity about the action
+//        mListener.onClosedBottomSheet(BOTTOM_SHEET_ADD_TAG, "some data");
+//        // Add the data into the DB
+//        this.createItem();
+//    }
+
+//    @OnClick(R.id.save_button)
+//    public void onClickSaveButton(){
+//        createItem();
+//    }
+
+    private void createItem(){
+//        Property property = new Property(this.editText.getText().toString(), this.spinner.getSelectedItemPosition(), USER_ID);
+//        this.editText.setText("");
+//        this.mPropertyViewModel.createProperty(property);
+
+        // Dismiss the fragment when finished
         dismiss();
     }
 
     //---------------
-    //  Set UI
+    //  UI
     //---------------
 
     private void setUiElements(){
-        test.setText(String.valueOf(currentPropertyID));
+
     }
+
+    private void alertDialogXButton(){
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with canceling the entry
+                        dismiss(); // Dismiss the bottomSheet fragment
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
 }
