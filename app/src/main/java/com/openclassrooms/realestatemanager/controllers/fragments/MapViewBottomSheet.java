f63d8fbfb7e7e49ce0800abbe3d6de3ca3db37e9
e210564e <devedelie@gmail.com>
2020-01-02 12:24:35 +0100

    - added some map configurations

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/MapViewBottomSheet.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/fragments/MapViewBottomSheet.java
@@ -1,28 +1,47 @@
 package com.openclassrooms.realestatemanager.controllers.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.base.BaseBottomSheet;
+
+import butterknife.ButterKnife;
+
+import static com.android.volley.VolleyLog.TAG;
+import static com.openclassrooms.realestatemanager.models.Constants.MAXIMUM_ZOOM_PREFERENCE;
+import static com.openclassrooms.realestatemanager.models.Constants.MINIMUM_ZOOM_PREFERENCE;
 
 /**
  * Created by Eliran Elbaz on 02-Jan-20.
  */
 public class MapViewBottomSheet extends BaseBottomSheet implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private GoogleMap mMap;
-    public static final String TITLE = "Map View";
+    private static final float DEFAULT_ZOOM = 15f ;
+
+    public static MapViewBottomSheet newInstance(int currentPropertyID) {
+        MapViewBottomSheet mapViewBottomSheet = new MapViewBottomSheet();
+        Bundle bundle = new Bundle();
+//        bundle.putInt(CURRENT_PROPERTY_ID, currentPropertyID);
+        mapViewBottomSheet.setArguments(bundle);
+        Log.d(TAG, "newInstance: reached" );
+        return mapViewBottomSheet;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(getFragmentLayout(), container, false);
+        ButterKnife.bind(this, view); //Configure Butterknife
 
         // Initialise map_view_fragment
         this.initMap();
@@ -31,7 +50,7 @@ public class MapViewBottomSheet extends BaseBottomSheet implements OnMapReadyCal
 
     // Initialise Google Map
     private void initMap(){
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -52,8 +71,23 @@ public class MapViewBottomSheet extends BaseBottomSheet implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
+        // Map-Zoom limitations
+        mMap.setMinZoomPreference(MINIMUM_ZOOM_PREFERENCE);
+        mMap.setMaxZoomPreference(MAXIMUM_ZOOM_PREFERENCE);
+        // Map configurations
+        mMap.setBuildingsEnabled(true);
+//        mMap.setMyLocationEnabled(true);
+//        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        moveCamera(new LatLng(40.644000, -74.004710), DEFAULT_ZOOM);
 
+    }
+
+    // A method to move the camera(map) to specific location by passing LatLng and Zoom
+    private void moveCamera(LatLng latLng, float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     @Override
