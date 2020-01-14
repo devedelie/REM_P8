ed2e340c0729413d544f0c2db9abd25bf16dcdc9
e210564e <devedelie@gmail.com>
2020-01-14 10:26:01 +0100

    - Override method for home button

--- app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/DetailActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/DetailActivity.java
@@ -51,12 +51,10 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if ( id == android.R.id.home ) {
             finish();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
