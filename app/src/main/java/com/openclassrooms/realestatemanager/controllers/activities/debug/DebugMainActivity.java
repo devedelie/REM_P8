b51f875049fbcbba60ce4156bf0455e74612f68e
e210564e <devedelie@gmail.com>
2020-01-19 14:52:52 +0100

    - Added the original MainActivity & SecondActivity java files for the exam - Fixed 2 bugs - Added a string

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/debug/DebugMainActivity.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.realestatemanager.controllers.activities.debug;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.utils.Utils;
+
+public class DebugMainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
+    private TextView textViewQuantity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        this.textViewMain = findViewById(R.id.debug_activity_second_activity_text_view_main);  // ERROR 1 ***********
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
+        this.textViewQuantity = findViewById(R.id.debug_activity_main_activity_text_view_quantity);
+
+        this.configureTextViewMain();
+        this.configureTextViewQuantity();
+    }
+
+    private void configureTextViewMain(){
+        this.textViewMain.setTextSize(15);
+        this.textViewMain.setText(getString(R.string.premier_bien));
+    }
+
+    private void configureTextViewQuantity(){
+        int quantity = Utils.convertDollarToEuro(100);
+        this.textViewQuantity.setTextSize(20);
+//        this.textViewQuantity.setText(quantity); // ERROR 2  *************************
+        this.textViewQuantity.setText(String.valueOf(quantity));
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/debug/DebugSecondActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.realestatemanager.controllers.activities.debug;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.openclassrooms.realestatemanager.R;
+
+public class DebugSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.debug_activity_second);
+    }
+}
