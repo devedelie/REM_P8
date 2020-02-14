a777cc54eb1448b135a69f4b984f36f704151495
e210564e <devedelie@gmail.com>
2020-02-14 23:47:17 +0100

    - Added network permissions to Manifest - Modified Connectivity test class to assert returning value, while enabling/disabling network state

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsTest.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.SystemClock;
 
 import androidx.test.core.app.ApplicationProvider;
 
@@ -8,21 +11,50 @@ import com.openclassrooms.realestatemanager.utils.Utils;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Eliran Elbaz on 19-Jan-20.
  */
 public class UtilsTest {
+    Context context = ApplicationProvider.getApplicationContext();
 
     @Test
-    public void isInternetAvailable () throws Exception {
-        Context context = ApplicationProvider.getApplicationContext();
-        if(Utils.isInternetAvailable(context)){
-            assertTrue(Utils.isInternetAvailable(context)); // Assert true
-        }else{
-            assertFalse(Utils.isInternetAvailable(context)); // Assert false when internet is off
-        }
-
+    public void isInternetAvailable_whenDisconnected_returnFalse() {
+        setWifiState(false);
+        SystemClock.sleep(5000); // Delay for network state change to take effect
+        assertFalse(Utils.isInternetAvailable(context)); // Assert false when internet is off
     }
+
+    @Test
+    public void isInternetAvailable_whenConnected_returnTrue() {
+        setWifiState(true);
+        SystemClock.sleep(5000);  // Delay for network state change to take effect
+        assertTrue(Utils.isInternetAvailable(context)); // Assert true
+    }
+
+
+    //----------------------
+    // Helper Methods
+    //----------------------
+
+    private void setWifiState(boolean state) {
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        wifi.setWifiEnabled(state);
+    }
+
+
+
+
+
+
+
+
+
+
 }
